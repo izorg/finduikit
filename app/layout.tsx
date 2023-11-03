3a0e52de0f8c9cc5d9517eaac038ff06c8de020f
Viacheslav Zavoruev <izorgster@gmail.com>
2023-11-03 20:28:07 +0100

    Enable auto dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+import { type Viewport } from "next";
 import { type PropsWithChildren } from "react";
 
 import "./global.css";
+
+export const viewport: Viewport = {
+  colorScheme: "light dark",
+};
 
 const RootLayout = (props: PropsWithChildren) => {
   const { children } = props;
