a5bc709d716437fa18113cecaa66f84c7ae817cb
Viacheslav Zavoruev <izorgster@gmail.com>
2025-01-26 22:20:17 +0100

    chore: use React 19 meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
 import "./global.css";
 import { Theme } from "@radix-ui/themes";
-import { type Viewport } from "next";
 import { ThemeProvider } from "next-themes";
 import { type PropsWithChildren } from "react";
-
-export const viewport: Viewport = {
-  colorScheme: "light dark",
-};
 
 const RootLayout = (props: PropsWithChildren) => {
   const { children } = props;
 
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta content="dark light" name="color-scheme" />
+      </head>
       <body>
         <ThemeProvider attribute="class">
           <Theme>{children}</Theme>
