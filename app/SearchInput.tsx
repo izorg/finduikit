aa313d53d62bf564ed787fddd60d0ecc2a09fc32
Viacheslav Zavoruev <izorgster@gmail.com>
2025-02-01 11:42:24 +0100

    chore: use TextField props from namespace

--- app/SearchInput.tsx
+++ app/SearchInput.tsx
@@ -2,14 +2,13 @@
 
 import { mdiMagnify } from "@mdi/js";
 import { TextField } from "@radix-ui/themes";
-import { type RootProps } from "@radix-ui/themes/components/text-field";
 
 import { useSearch } from "./SearchProvider";
 import { SvgIcon } from "./SvgIcon";
 
 const placeholder = "Search by name";
 
-export const SearchInput = (props: RootProps) => {
+export const SearchInput = (props: TextField.RootProps) => {
   const { setSearch } = useSearch();
 
   return (
