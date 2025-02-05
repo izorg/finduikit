78155ed8de722fbcbeb1a41f0894fca84a34eada
Viacheslav Zavoruev <izorgster@gmail.com>
2025-02-05 21:37:00 +0100

    chore: update Stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,18 +2,14 @@ export default {
   extends: ["stylelint-config-standard", "stylelint-config-css-modules"],
   plugins: ["stylelint-no-unsupported-browser-features", "stylelint-order"],
   rules: {
-    "custom-property-empty-line-before": null,
-
-    "function-no-unknown": [
-      true,
-      {
-        ignoreFunctions: ["theme"],
-      },
-    ],
-
     "order/properties-alphabetical-order": true,
 
-    "plugin/no-unsupported-browser-features": true,
+    "plugin/no-unsupported-browser-features": [
+      true,
+      {
+        ignore: ["css-logical-props"],
+      },
+    ],
 
     "selector-class-pattern": null,
   },
