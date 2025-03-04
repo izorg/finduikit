9a41e5e0bb3f7537f09f3bfd844bc1c0afa2beae
Viacheslav Zavoruev <izorgster@gmail.com>
2025-03-04 23:00:20 +0100

    chore: generate JSON schema from zod

---
+++ domains/ui-kit/scripts/generate-json-schema.ts
@@ -0,0 +1,23 @@
+import fs from "node:fs";
+import path from "node:path";
+
+import { ESLint } from "eslint";
+import prettier from "prettier";
+import { zodToJsonSchema } from "zod-to-json-schema";
+
+import { uiKitSchema } from "../uiKitSchema";
+
+const jsonSchema = zodToJsonSchema(uiKitSchema, "UiKit");
+const output = JSON.stringify(jsonSchema, undefined, 2);
+const eslint = new ESLint({ fix: true });
+const filePath = path.join(process.cwd(), "domains/ui-kit/UiKit.schema.json");
+
+const lintResults = await eslint.lintText(output, {
+  filePath,
+});
+const fixedOutput = lintResults[0].output ?? output;
+const formattedOutput = await prettier.format(fixedOutput, {
+  filepath: filePath,
+});
+
+await fs.promises.writeFile(filePath, formattedOutput);
