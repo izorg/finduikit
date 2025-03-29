5ce3d3612e451293177810fa2a1e62921e7705c6
Viacheslav Zavoruev <izorgster@gmail.com>
2025-03-29 11:46:37 +0100

    chore: refactor getUiKits to separate file data and dynamic data retrieval

--- domains/ui-kit/getUiKits.ts
+++ domains/ui-kit/getUiKits.ts
@@ -1,16 +1,20 @@
 import fs from "node:fs";
 import path from "node:path";
 
+import { captureException } from "@sentry/nextjs";
 import { parse } from "yaml";
 
 import { firebaseGetFirestoreUiKitsCollection } from "../firebase";
 
-import { type UiKitDynamicDataSchema } from "./uiKitDynamicDataSchema";
+import {
+  uiKitDynamicDataSchema,
+  type UiKitDynamicDataSchema,
+} from "./uiKitDynamicDataSchema";
 import { uiKitSchema, type UiKitSchema } from "./uiKitSchema";
 
 export type UiKit = UiKitDynamicDataSchema & UiKitSchema;
 
-export const getUiKits = async () => {
+const getUiKitFileDataEntriesFromFiles = async () => {
   const entries = await fs.promises.readdir(
     path.join(process.cwd(), "ui-kits"),
     {
@@ -18,34 +22,56 @@ export const getUiKits = async () => {
     },
   );
 
-  const uiKitsCollection = firebaseGetFirestoreUiKitsCollection();
-
-  let uiKitsDynamicData: Partial<Record<string, UiKitDynamicDataSchema>>;
-
-  try {
-    const snapshot = await uiKitsCollection.get();
-
-    uiKitsDynamicData = Object.fromEntries(
-      snapshot.docs.map((doc) => [doc.id, doc.data()]),
-    );
-  } catch {
-    uiKitsDynamicData = {};
-  }
-
   return await Promise.all(
     entries
       .filter((dirent) => dirent.isFile())
-      .map(async (dirent) => {
+      .map(async (dirent): Promise<[key: string, data: UiKitSchema]> => {
         const buffer = await fs.promises.readFile(
           path.join(dirent.parentPath, dirent.name),
         );
 
-        const dynamicData = uiKitsDynamicData[dirent.name];
-
-        return {
-          ...uiKitSchema.parse(parse(buffer.toString())),
-          ...dynamicData,
-        };
+        return [dirent.name, uiKitSchema.parse(parse(buffer.toString()))];
       }),
   );
+};
+
+const getUiKitDynamicDataMapFromFirestore = async () => {
+  const uiKitsCollection = firebaseGetFirestoreUiKitsCollection();
+
+  const uiKitsDynamicDataMap = new Map<string, UiKitDynamicDataSchema>();
+
+  try {
+    const snapshot = await uiKitsCollection.get();
+
+    for (const doc of snapshot.docs) {
+      uiKitsDynamicDataMap.set(
+        doc.id,
+        uiKitDynamicDataSchema.parse(doc.data()),
+      );
+    }
+  } catch (error) {
+    captureException(error);
+  }
+
+  return uiKitsDynamicDataMap;
+};
+
+const nameCompare = new Intl.Collator("en").compare;
+
+export const getUiKits = async () => {
+  const [uiKitFileDataEntries, uiKitDynamicDataMap] = await Promise.all([
+    getUiKitFileDataEntriesFromFiles(),
+    getUiKitDynamicDataMapFromFirestore(),
+  ]);
+
+  return uiKitFileDataEntries
+    .map(([key, fileData]) => {
+      const dynamicData = uiKitDynamicDataMap.get(key);
+
+      return {
+        ...fileData,
+        ...dynamicData,
+      };
+    })
+    .toSorted((a, b) => nameCompare(a.name, b.name));
 };
