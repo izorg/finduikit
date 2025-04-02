9ac8ded2edb9d48d3b108bcf49b8ee16c4e4f516
Viacheslav Zavoruev <izorgster@gmail.com>
2025-04-02 13:16:14 +0200

    feat: add polyfill for Array.prototype.toSorted

--- domains/polyfills/index.ts
+++ domains/polyfills/index.ts
@@ -4,4 +4,5 @@
  * Next.js provides polyfills https://github.com/vercel/next.js/blob/canary/packages/next-polyfill-nomodule/src/index.js
  * but for better Developer Experience we provide more of them
  */
+import "core-js/features/array/to-sorted";
 import "core-js/features/object/from-entries";
