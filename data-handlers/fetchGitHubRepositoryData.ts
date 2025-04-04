15a168e78ba748de1a9a494ba76b36b914b80e07
Viacheslav Zavoruev <izorgster@gmail.com>
2025-04-04 08:06:02 +0200

    chore: refactor GitHub data fetching to improve query structure and error handling

--- data-handlers/fetchGitHubRepositoryData.ts
+++ data-handlers/fetchGitHubRepositoryData.ts
@@ -4,43 +4,45 @@ import type {
 } from "./fetchGitHubRepositoryData.generated";
 
 export const fetchGitHubRepositoryData = async (url: string) => {
+  const query = /* GraphQL */ `
+    query getGitHubRepository($url: URI!) {
+      resource(url: $url) {
+        __typename
+        ... on Repository {
+          defaultBranchRef {
+            target {
+              ... on Commit {
+                __typename
+                committedDate
+              }
+            }
+          }
+          description
+          homepageUrl
+          issues(states: [OPEN]) {
+            totalCount
+          }
+          openGraphImageUrl
+          repositoryTopics(first: 100) {
+            nodes {
+              topic {
+                name
+              }
+            }
+          }
+          stargazerCount
+        }
+      }
+    }
+  `;
+
   const variables: GetGitHubRepositoryQueryVariables = {
     url,
   };
 
   const response = await fetch("https://api.github.com/graphql", {
     body: JSON.stringify({
-      query: /* GraphQL */ `
-        query getGitHubRepository($url: URI!) {
-          resource(url: $url) {
-            __typename
-            ... on Repository {
-              defaultBranchRef {
-                target {
-                  ... on Commit {
-                    __typename
-                    committedDate
-                  }
-                }
-              }
-              description
-              homepageUrl
-              issues(states: [OPEN]) {
-                totalCount
-              }
-              openGraphImageUrl
-              repositoryTopics(first: 100) {
-                nodes {
-                  topic {
-                    name
-                  }
-                }
-              }
-              stargazerCount
-            }
-          }
-        }
-      `,
+      query,
       variables,
     }),
     headers: {
@@ -51,6 +53,10 @@ export const fetchGitHubRepositoryData = async (url: string) => {
   });
 
   const json = await response.json();
+
+  if (json.errors) {
+    throw new Error(JSON.stringify(json.errors));
+  }
 
   const data = json.data as GetGitHubRepositoryQuery;
 
