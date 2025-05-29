eb944d03b3860db507e539768e92e2bf18ba6629
Viacheslav Zavoruev <izorgster@gmail.com>
2025-05-29 08:20:44 +0200

    feat: sticky header

--- domains/compare-page/ComparePage/ComparePage.tsx
+++ domains/compare-page/ComparePage/ComparePage.tsx
@@ -12,6 +12,8 @@ import { SvgIcon } from "../../icon";
 import { getUiKits } from "../../ui-kit";
 import { ComparePageFrameworkList } from "../ComparePageFrameworkList";
 
+import styles from "./ComparePage.module.css";
+
 const title = "UI Kits Comparison Table";
 
 export const metadata: Metadata = {
@@ -33,77 +35,79 @@ export const ComparePage = async () => {
   return (
     <Flex direction="column" gap="4" pb="4">
       <Container size="2">
-        <Table.Root size="3">
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell justify="center">
-                Framework
-              </Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell justify="center">
-                Styled
-              </Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell justify="center">
-                Figma
-              </Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell justify="center">
-                Storybook
-              </Table.ColumnHeaderCell>
-            </Table.Row>
-          </Table.Header>
-
-          <Table.Body>
-            {sortedUiKits.map((uiKit) => (
-              <Table.Row key={uiKit.name}>
-                <Table.RowHeaderCell>
-                  <Link asChild color="gray" highContrast>
-                    <a href={uiKit.homepage} rel="noreferrer" target="_blank">
-                      {uiKit.name}
-                    </a>
-                  </Link>
-                </Table.RowHeaderCell>
-                <Table.Cell justify="center">
-                  <ComparePageFrameworkList frameworks={uiKit.frameworks} />
-                </Table.Cell>
-                <Table.Cell justify="center">
-                  {!uiKit.unstyled && (
-                    <Text color="jade">
-                      <SvgIcon path={mdiCheck} />
-                    </Text>
-                  )}
-                </Table.Cell>
-                <Table.Cell justify="center">
-                  {uiKit.figma && (
-                    <Text asChild color="crimson">
-                      <a
-                        aria-label="Figma"
-                        href={uiKit.figma}
-                        rel="noreferrer"
-                        target="_blank"
-                      >
-                        <SvgIcon path={siFigma.path} />
-                      </a>
-                    </Text>
-                  )}
-                </Table.Cell>
-                <Table.Cell justify="center">
-                  {uiKit.storybook && (
-                    <Text asChild color="ruby">
-                      <a
-                        aria-label="Storybook"
-                        href={uiKit.storybook}
-                        rel="noreferrer"
-                        target="_blank"
-                      >
-                        <SvgIcon path={siStorybook.path} />
-                      </a>
-                    </Text>
-                  )}
-                </Table.Cell>
+        <div className="rt-TableRoot rt-r-size-3 rt-variant-ghost">
+          <table className="rt-TableRootTable">
+            <Table.Header className={styles.tableHead}>
+              <Table.Row>
+                <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell justify="center">
+                  Framework
+                </Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell justify="center">
+                  Styled
+                </Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell justify="center">
+                  Figma
+                </Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell justify="center">
+                  Storybook
+                </Table.ColumnHeaderCell>
               </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
+            </Table.Header>
+
+            <Table.Body>
+              {sortedUiKits.map((uiKit) => (
+                <Table.Row key={uiKit.name}>
+                  <Table.RowHeaderCell>
+                    <Link asChild color="gray" highContrast>
+                      <a href={uiKit.homepage} rel="noreferrer" target="_blank">
+                        {uiKit.name}
+                      </a>
+                    </Link>
+                  </Table.RowHeaderCell>
+                  <Table.Cell justify="center">
+                    <ComparePageFrameworkList frameworks={uiKit.frameworks} />
+                  </Table.Cell>
+                  <Table.Cell justify="center">
+                    {!uiKit.unstyled && (
+                      <Text color="jade">
+                        <SvgIcon path={mdiCheck} />
+                      </Text>
+                    )}
+                  </Table.Cell>
+                  <Table.Cell justify="center">
+                    {uiKit.figma && (
+                      <Text asChild color="crimson">
+                        <a
+                          aria-label="Figma"
+                          href={uiKit.figma}
+                          rel="noreferrer"
+                          target="_blank"
+                        >
+                          <SvgIcon path={siFigma.path} />
+                        </a>
+                      </Text>
+                    )}
+                  </Table.Cell>
+                  <Table.Cell justify="center">
+                    {uiKit.storybook && (
+                      <Text asChild color="ruby">
+                        <a
+                          aria-label="Storybook"
+                          href={uiKit.storybook}
+                          rel="noreferrer"
+                          target="_blank"
+                        >
+                          <SvgIcon path={siStorybook.path} />
+                        </a>
+                      </Text>
+                    )}
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </table>
+        </div>
       </Container>
 
       <Footer />
