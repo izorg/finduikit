5e49c426406b278d8ad43134944d1b51791ce02f
Viacheslav Zavoruev <izorgster@gmail.com>
2025-05-31 10:59:29 +0200

    chore: drop static search page generation

--- app/search/[[...slug]]/page.tsx
+++ app/search/[[...slug]]/page.tsx
@@ -1,23 +1,3 @@
-import { frameworkParams } from "../../../domains/framework";
-
-export const revalidate = 3600;
-
-export const dynamicParams = false;
-
-export const generateStaticParams = () => [
-  ...Object.values(frameworkParams)
-    .map((framework) => ({
-      slug: [framework],
-    }))
-    .flatMap(({ slug }) => [{ slug }, { slug: [...slug, "unstyled"] }]),
-  {
-    slug: undefined,
-  },
-  {
-    slug: ["unstyled"],
-  },
-];
-
 export {
   Page as default,
   generateMetadata,
