b171f24a3e240c9fbcdf082b2d363abcf9fc6173
Viacheslav Zavoruev <izorgster@gmail.com>
2025-06-18 23:04:48 +0200

    feat: add UI kit stats cards

--- domains/home-page/Page/Page.tsx
+++ domains/home-page/Page/Page.tsx
@@ -1,11 +1,14 @@
-import { mdiTelescope } from "@mdi/js";
-import { Button, Flex, Heading, Text } from "@radix-ui/themes";
+import { mdiRhombusSplit, mdiTelescope } from "@mdi/js";
+import { Button, Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { type Metadata } from "next";
 import NextLink from "next/link";
+import { siFigma, siStorybook } from "simple-icons";
 
 import { Footer } from "../../footer";
 import { SvgIcon } from "../../icon";
 import { getUiKits } from "../../ui-kit/server";
+
+import styles from "./Page.module.css";
 
 const title = "UI Kits";
 const description =
@@ -22,7 +25,7 @@ export const Page = async () => {
   const uiKitSet = await uiKitsResource;
 
   return (
-    <Flex direction="column" gap="6" minHeight="100dvh" py="4">
+    <Flex align="center" direction="column" gap="6" minHeight="100dvh" py="4">
       <Flex asChild direction="column" gap="2" px="4">
         <Text align="center" asChild>
           <header>
@@ -42,11 +45,48 @@ export const Page = async () => {
           </header>
         </Text>
       </Flex>
-      <Flex align="center" asChild direction="column">
-        <Text as="p">
-          <Text size="8">{uiKitSet.size}</Text>
-          <Text>kits</Text>
-        </Text>
+      <Flex gap="4" justify="center" px="4" wrap="wrap">
+        <Card className={styles.card} size="2">
+          <Text asChild color="purple">
+            <SvgIcon
+              className={styles.cardIllustration}
+              path={mdiRhombusSplit}
+            />
+          </Text>
+          <Text as="div" color="gray" size="2">
+            Total UI kits
+          </Text>
+          <Text as="div" size="7" weight="medium">
+            {uiKitSet.size.toLocaleString()}
+          </Text>
+        </Card>
+        <Card className={styles.card} size="2">
+          <Text asChild color="ruby">
+            <SvgIcon
+              className={styles.cardIllustration}
+              path={siStorybook.path}
+            />
+          </Text>
+          <Text as="div" color="gray" size="2">
+            Storybooks
+          </Text>
+          <Text as="div" size="7" weight="medium">
+            {[...uiKitSet]
+              .filter((item) => item.storybook)
+              .length.toLocaleString()}
+          </Text>
+        </Card>
+        <Card className={styles.card} size="2">
+          <Text asChild color="crimson">
+            <SvgIcon className={styles.cardIllustration} path={siFigma.path} />
+          </Text>
+          <Text as="div" color="gray" size="2">
+            Figma files
+          </Text>
+          <Text as="div" size="7" weight="medium">
+            {[...uiKitSet].filter((item) => item.figma).length.toLocaleString()}
+          </Text>
+        </Card>
       </Flex>
       <Flex
         align="center"
