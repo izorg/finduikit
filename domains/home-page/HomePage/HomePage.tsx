59b5e962ff2fb2ffd663e798dec5c11271e257d7
Viacheslav Zavoruev <izorgster@gmail.com>
2025-07-14 22:14:28 +0200

    feat: add selling points

--- domains/home-page/HomePage/HomePage.tsx
+++ domains/home-page/HomePage/HomePage.tsx
@@ -4,7 +4,7 @@ import {
   mdiRhombusSplit,
   mdiTelescope,
 } from "@mdi/js";
-import { Button, Flex, Heading, Text } from "@radix-ui/themes";
+import { Button, Container, Flex, Heading, Text } from "@radix-ui/themes";
 import { type Metadata } from "next";
 import NextLink from "next/link";
 import { siFigma, siStorybook } from "simple-icons";
@@ -28,81 +28,98 @@ export const HomePage = async () => {
   const uiKitSet = await getUiKits();
 
   return (
-    <Flex
-      align="center"
-      direction="column"
-      gap={{
-        initial: "6",
-        sm: "9",
-      }}
-      minHeight="100dvh"
-      py="4"
-    >
-      <Flex asChild direction="column" gap="2" px="4">
-        <Text align="center" asChild>
-          <header>
-            <Heading
-              as="h1"
-              size={{
-                initial: "8",
-                sm: "9",
-              }}
-              weight="medium"
-            >
-              {title}
-            </Heading>
-            <Text as="p" size="4">
-              {description}
-            </Text>
-          </header>
-        </Text>
-      </Flex>
-      <Flex gap="4" justify="center" px="4" wrap="wrap">
-        <Button asChild size="4">
-          <NextLink href="/explore">
-            <SvgIcon aria-hidden path={mdiTelescope} />
-            Explore
-          </NextLink>
-        </Button>
-        <SupportButton size="4" variant="soft">
-          <SvgIcon aria-hidden path={mdiMessageOutline} />
-          Support
-        </SupportButton>
-      </Flex>
+    <Container size="3">
       <Flex
-        gap={{ initial: "4", md: "8", sm: "6" }}
-        justify="center"
-        px="4"
-        wrap="wrap"
+        align="center"
+        direction="column"
+        gap={{
+          initial: "6",
+          sm: "9",
+        }}
+        minHeight="100dvh"
+        p="4"
       >
-        <StatCard>
-          <StatCard.Icon color="purple" path={mdiRhombusSplit} />
-          <StatCard.Label>Total UI kits</StatCard.Label>
-          <StatCard.Stat>{uiKitSet.size}</StatCard.Stat>
-        </StatCard>
-        <StatCard>
-          <StatCard.Icon color="ruby" path={siStorybook.path} />
-          <StatCard.Label>Storybooks</StatCard.Label>
-          <StatCard.Stat>
-            {[...uiKitSet].filter((item) => item.storybook).length}
-          </StatCard.Stat>
-        </StatCard>
-        <StatCard>
-          <StatCard.Icon color="crimson" path={siFigma.path} />
-          <StatCard.Label>Figma files</StatCard.Label>
-          <StatCard.Stat>
-            {[...uiKitSet].filter((item) => item.figma).length}
-          </StatCard.Stat>
-        </StatCard>
-        <StatCard>
-          <StatCard.Icon color="crimson" path={mdiCreation} />
-          <StatCard.Label>Build with AI</StatCard.Label>
-          <StatCard.Stat>
-            {[...uiKitSet].filter((item) => item.ai).length}
-          </StatCard.Stat>
-        </StatCard>
+        <Flex asChild direction="column" gap="2">
+          <Text align="center" asChild>
+            <header>
+              <Heading
+                as="h1"
+                size={{
+                  initial: "8",
+                  sm: "9",
+                }}
+                weight="medium"
+              >
+                {title}
+              </Heading>
+              <Text as="p" size="4">
+                {description}
+              </Text>
+            </header>
+          </Text>
+        </Flex>
+        <Flex gap="4" justify="center" wrap="wrap">
+          <Button asChild size="4">
+            <NextLink href="/explore">
+              <SvgIcon aria-hidden path={mdiTelescope} />
+              Explore
+            </NextLink>
+          </Button>
+          <SupportButton size="4" variant="soft">
+            <SvgIcon aria-hidden path={mdiMessageOutline} />
+            Support
+          </SupportButton>
+        </Flex>
+        <Flex gap="4" justify="center" wrap="wrap">
+          <StatCard>
+            <StatCard.Icon color="purple" path={mdiRhombusSplit} />
+            <StatCard.Label>Total UI kits</StatCard.Label>
+            <StatCard.Stat>{uiKitSet.size}</StatCard.Stat>
+          </StatCard>
+          <StatCard>
+            <StatCard.Icon color="ruby" path={siStorybook.path} />
+            <StatCard.Label>Storybooks</StatCard.Label>
+            <StatCard.Stat>
+              {[...uiKitSet].filter((item) => item.storybook).length}
+            </StatCard.Stat>
+          </StatCard>
+          <StatCard>
+            <StatCard.Icon color="crimson" path={siFigma.path} />
+            <StatCard.Label>Figma files</StatCard.Label>
+            <StatCard.Stat>
+              {[...uiKitSet].filter((item) => item.figma).length}
+            </StatCard.Stat>
+          </StatCard>
+          <StatCard>
+            <StatCard.Icon color="crimson" path={mdiCreation} />
+            <StatCard.Label>Build with AI</StatCard.Label>
+            <StatCard.Stat>
+              {[...uiKitSet].filter((item) => item.ai).length}
+            </StatCard.Stat>
+          </StatCard>
+        </Flex>
+        <section>
+          <Heading mb="2" weight="medium">
+            Choose Tech with UI Kits
+          </Heading>
+          <Text>
+            Knowing UI kits helps teams pick the best tools for business. It
+            keeps designs consistent, and improves teamwork between designers
+            and developers.
+          </Text>
+        </section>
+        <section>
+          <Heading mb="2" weight="medium">
+            Speed Up Development
+          </Heading>
+          <Text>
+            Selecting the right UI kit accelerates development by providing
+            ready-made components, reducing repetitive work, and allowing teams
+            to focus on building unique features.
+          </Text>
+        </section>
+        <Footer mt="auto" />
       </Flex>
-      <Footer mt="auto" />
-    </Flex>
+    </Container>
   );
 };
