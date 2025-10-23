7b85d8119da39769d7142c4c08e942bbff9928a8
Viacheslav Zavoruev <izorgster@gmail.com>
2025-10-23 09:25:20 +0200

    fix: revert TS enable

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,9 @@ const nextConfig: NextConfig = {
   },
   reactCompiler: true,
   typedRoutes: true,
+  typescript: {
+    ignoreBuildErrors: true, // Using project root TypeScript check
+  },
 };
 
 const sentryBuildOptions: SentryBuildOptions = {
