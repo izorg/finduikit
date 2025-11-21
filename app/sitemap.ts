6327498aadd5165fa388e92399aa2f752ec97df0
Viacheslav Zavoruev <izorgster@gmail.com>
2025-11-21 21:55:31 +0100

    feat: update sitemap configuration for static generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,21 @@
+export const dynamic = "force-static";
+
+export const revalidate = false;
+
 import type { MetadataRoute } from "next";
+
+const lastModified = new Date();
 
 const sitemap = (): MetadataRoute.Sitemap => [
   {
     changeFrequency: "daily",
-    lastModified: new Date(),
+    lastModified,
     priority: 1,
     url: "https://finduikit.com",
   },
   {
     changeFrequency: "daily",
-    lastModified: new Date(),
+    lastModified,
     priority: 1,
     url: "https://finduikit.com/explore",
   },
