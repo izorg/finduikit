7846b8537525ceaaf6db1e29b8f38026db6a24ef
Viacheslav Zavoruev <izorgster@gmail.com>
2025-11-26 23:16:16 +0100

    chore: use `main` on homepage

--- domains/home-page/HomePage/HomePage.tsx
+++ domains/home-page/HomePage/HomePage.tsx
@@ -58,66 +58,78 @@ export const HomePage = async () => {
             </header>
           </Text>
         </Flex>
-        <Flex gap="4" justify="center" wrap="wrap">
-          <Button asChild size="4">
-            <NextLink href="/explore">
-              <SvgIcon aria-hidden path={mdiTelescope} />
-              Explore
-            </NextLink>
-          </Button>
-          <SupportButton size="4" variant="soft">
-            <SvgIcon aria-hidden path={mdiMessageOutline} />
-            Support
-          </SupportButton>
+        <Flex
+          align="center"
+          asChild
+          direction="column"
+          gap={{
+            initial: "6",
+            sm: "9",
+          }}
+        >
+          <main>
+            <Flex gap="4" justify="center" wrap="wrap">
+              <Button asChild size="4">
+                <NextLink href="/explore">
+                  <SvgIcon aria-hidden path={mdiTelescope} />
+                  Explore
+                </NextLink>
+              </Button>
+              <SupportButton size="4" variant="soft">
+                <SvgIcon aria-hidden path={mdiMessageOutline} />
+                Support
+              </SupportButton>
+            </Flex>
+            <Flex gap="4" justify="center" wrap="wrap">
+              <StatCard>
+                <StatCard.Icon color="purple" path={mdiRhombusSplit} />
+                <StatCard.Label>Total UI kits</StatCard.Label>
+                <StatCard.Stat>{uiKitSet.size}</StatCard.Stat>
+              </StatCard>
+              <StatCard>
+                <StatCard.Icon color="ruby" path={siStorybook.path} />
+                <StatCard.Label>Storybooks</StatCard.Label>
+                <StatCard.Stat>
+                  {[...uiKitSet].filter((item) => item.storybook).length}
+                </StatCard.Stat>
+              </StatCard>
+              <StatCard>
+                <StatCard.Icon color="crimson" path={siFigma.path} />
+                <StatCard.Label>Figma files</StatCard.Label>
+                <StatCard.Stat>
+                  {[...uiKitSet].filter((item) => item.figma).length}
+                </StatCard.Stat>
+              </StatCard>
+              <StatCard>
+                <StatCard.Icon color="crimson" path={mdiCreation} />
+                <StatCard.Label>Build with AI</StatCard.Label>
+                <StatCard.Stat>
+                  {[...uiKitSet].filter((item) => item.ai).length}
+                </StatCard.Stat>
+              </StatCard>
+            </Flex>
+            <section>
+              <Heading mb="2" weight="medium">
+                Choose Tech with UI Kits
+              </Heading>
+              <Text>
+                Knowing UI kits helps teams pick the best tools for business. It
+                keeps designs consistent, and improves teamwork between
+                designers and developers.
+              </Text>
+            </section>
+            <section>
+              <Heading mb="2" weight="medium">
+                Speed Up Development
+              </Heading>
+              <Text>
+                Selecting the right UI kit accelerates development by providing
+                ready-made components, reducing repetitive work, and allowing
+                teams to focus on building unique features.
+              </Text>
+            </section>
+          </main>
         </Flex>
-        <Flex gap="4" justify="center" wrap="wrap">
-          <StatCard>
-            <StatCard.Icon color="purple" path={mdiRhombusSplit} />
-            <StatCard.Label>Total UI kits</StatCard.Label>
-            <StatCard.Stat>{uiKitSet.size}</StatCard.Stat>
-          </StatCard>
-          <StatCard>
-            <StatCard.Icon color="ruby" path={siStorybook.path} />
-            <StatCard.Label>Storybooks</StatCard.Label>
-            <StatCard.Stat>
-              {[...uiKitSet].filter((item) => item.storybook).length}
-            </StatCard.Stat>
-          </StatCard>
-          <StatCard>
-            <StatCard.Icon color="crimson" path={siFigma.path} />
-            <StatCard.Label>Figma files</StatCard.Label>
-            <StatCard.Stat>
-              {[...uiKitSet].filter((item) => item.figma).length}
-            </StatCard.Stat>
-          </StatCard>
-          <StatCard>
-            <StatCard.Icon color="crimson" path={mdiCreation} />
-            <StatCard.Label>Build with AI</StatCard.Label>
-            <StatCard.Stat>
-              {[...uiKitSet].filter((item) => item.ai).length}
-            </StatCard.Stat>
-          </StatCard>
-        </Flex>
-        <section>
-          <Heading mb="2" weight="medium">
-            Choose Tech with UI Kits
-          </Heading>
-          <Text>
-            Knowing UI kits helps teams pick the best tools for business. It
-            keeps designs consistent, and improves teamwork between designers
-            and developers.
-          </Text>
-        </section>
-        <section>
-          <Heading mb="2" weight="medium">
-            Speed Up Development
-          </Heading>
-          <Text>
-            Selecting the right UI kit accelerates development by providing
-            ready-made components, reducing repetitive work, and allowing teams
-            to focus on building unique features.
-          </Text>
-        </section>
         <Footer mt="auto" />
       </Flex>
     </Container>
