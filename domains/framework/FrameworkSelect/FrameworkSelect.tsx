e15de95d6dcd10bc87fc74fe41c1ce14bf6ffc2c
Viacheslav Zavoruev <izorgster@gmail.com>
2025-12-22 23:21:34 +0100

    feat: enhance FrameworkSelect with icons

--- domains/framework/FrameworkSelect/FrameworkSelect.tsx
+++ domains/framework/FrameworkSelect/FrameworkSelect.tsx
@@ -1,17 +1,35 @@
-import { Box, Select } from "@radix-ui/themes";
+import { Box, Flex, Select, Text } from "@radix-ui/themes";
 
+import { SvgIcon } from "../../icon";
 import { Framework } from "../Framework";
+import { frameworkColors } from "../frameworkColors";
+import { frameworkIcons } from "../frameworkIcons";
 
 const ANY_FRAMEWORK_LABEL = "Any framework";
 
 export const FrameworkSelect = (props: Select.RootProps) => {
   const { value = ANY_FRAMEWORK_LABEL, ...rest } = props;
 
+  const icon = Object.entries(frameworkIcons).find(
+    ([key]) => key === value,
+  )?.[1];
+
+  const color = Object.entries(frameworkColors).find(
+    ([key]) => key === value,
+  )?.[1];
+
   return (
     <Select.Root value={value} {...rest}>
       <Box asChild minWidth={{ initial: "140px", md: "170px" }}>
         <Select.Trigger aria-label="Select supported framework">
-          {value}
+          <Flex align="center" as="span" gap="2">
+            {icon && (
+              <Text asChild color={color}>
+                <SvgIcon path={icon.path} />
+              </Text>
+            )}
+            {value}
+          </Flex>
         </Select.Trigger>
       </Box>
       <Select.Content>
